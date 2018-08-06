ee0b50155ad4fa7624d619e63f2d24132c0f1496
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-05 22:28:50 -0400

    Selenium WebDriver with C# - find element by Name

---
+++ NameSelector/NameSelector/NameSelector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com/selectors/name/");
+
+        IWebElement element = driver.FindElement(By.Name("myName"));
+
+        if (element.Displayed)
+        {
+            System.Console.WriteLine("Yes I can see the element!!");
+            //GreenMessage("Yes I can see the element!!");
+        }
+        else
+        {
+            System.Console.WriteLine("No I cannot see the element!!");
+            //RedMessage("No I cannot see the element!!");
+        }
+
+        driver.Quit();
+    }
+
+    //    private static void RedMessage(string message)
+    //    {
+    //        Console.ForegroundColor = ConsoleColor.Red;
+    //        Console.WriteLine(message);
+    //        Console.ForegroundColor = ConsoleColor.White;
+    //    }
+
+    //    private static void GreenMessage(string message)
+    //    {
+    //        Console.ForegroundColor = ConsoleColor.Green;
+    //        Console.WriteLine(message);
+    //        Console.ForegroundColor = ConsoleColor.White;
+    //    }
+}
+
