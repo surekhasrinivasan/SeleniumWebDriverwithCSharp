f06f4db287b46d3f0db83c9fb47d295e6d5fa778
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-05 22:27:05 -0400

    Selenium WebDriver with C# - Set up Environment

---
+++ SetUpEnvironment/SetUpEnvironment/SetUpEnvironment/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
