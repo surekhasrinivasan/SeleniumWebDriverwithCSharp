fcdeb189091c9171207845c39c0002a00f7ad151
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-06 21:36:25 -0400

    Selenium WebDriver with C# - find element by Id

---
+++ IDSelector/IDSelector/Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "http://testing.todorvachev.com/selectors/id/";
+        string ID = "testImage";
+        
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.Id(ID));
+
+        if (element.Displayed)
+        {
+            GreenMessage("Yes, I can see the element");
+        }
+        else
+        {
+            RedMessage("No, I cannot see the element");
+        }
+
+        driver.Quit();
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
