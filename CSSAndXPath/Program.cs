169d1bf9e1e789c6c686995188c2479daa409387
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-08 21:51:49 -0400

    Selenium WebDriver with C# - find element by CSS and XPath

---
+++ CSSAndXPath/Program.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "http://testing.todorvachev.com/selectors/css-path/";
+        string cssPath = "#post-108 > div > figure > img";
+        string xPath = "//*[@id=\"post-108\"]/div/figure/img";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+        IWebElement xPathElement = driver.FindElement(By.XPath(xPath));
+
+        if (cssPathElement.Displayed)
+        {
+            GreenMessage("I can see the CSS Path element");
+        }
+        else
+        {
+            RedMessage("I cannot see the CSS Path element");
+        }
+
+        if (xPathElement.Displayed)
+        {
+            GreenMessage("I can see the XPath element");
+        }
+        else
+        {
+            RedMessage("I cannot see the XPath element");
+        }
+
+        driver.Quit();
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
