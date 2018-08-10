3d6a7fd05733811a8c8bf1d7c3438b196f38544a
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-10 15:23:33 -0400

    SeleniumWebdriver with C# - Special Elements - Input Text Box

---
+++ InputTextBox/InputTextBox/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class Program
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+
+        textBox.SendKeys("Test text");
+
+        Thread.Sleep(3000);
+
+        //textBox.Clear(); //clears the input text in the text box
+
+        Console.WriteLine(textBox.GetAttribute("value"));
+        Console.WriteLine(textBox.GetAttribute("maxlength")); //gives the maximum number of characters the text box can take
+        
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
