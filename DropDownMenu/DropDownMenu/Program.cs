362a80b59c00f940924f95c49f9fb65766e0a43a
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-14 13:09:44 -0400

    SeleniumWebdriver with C# - Special Elements - DropDownMenu element

---
+++ DropDownMenu/DropDownMenu/Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class Program
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement dropDownMenu;
+    static IWebElement elementFromDropDownMenu;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/drop-down-menu-test/";
+        string dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(3)";
+        
+        driver.Navigate().GoToUrl(url);
+
+        dropDownMenu = driver.FindElement(By.Name("DropDownTest"));
+
+        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
+
+        elementFromDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
+
+        Console.WriteLine("The third option from the drop down menu is: " + elementFromDropDownMenu.GetAttribute("value"));
+
+        elementFromDropDownMenu.Click();
+
+        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
+        
+        Thread.Sleep(3000);
+        
+        for(int i = 1; i <= 4; i++)
+        {
+
+            dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(" + i + ")";
+            elementFromDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
+
+            Console.WriteLine("The " + i + " option from the drop down menu is: " + elementFromDropDownMenu.GetAttribute("value"));
+        }
+
+        Thread.Sleep(15000);
+
+        driver.Quit();
+    }
+}
