7dade8024e850029d1e0d32796ebdd40f61c510b
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-14 13:12:11 -0400

    SeleniumWebdriver with C# - Special Elements - AlertBox element

---
+++ AlertBox/AlertBox/Program.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class Program
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IAlert alert;
+    static IWebElement image;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/alert-box/";
+
+        driver.Navigate().GoToUrl(url);
+
+        alert = driver.SwitchTo().Alert();
+
+        Console.WriteLine(alert.Text);
+
+        alert.Accept();
+
+        image = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
+
+        try
+        {
+            if (image.Displayed)
+            {
+                Console.WriteLine("The alert was successfully accepted and I can see the image");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("Sorry something went wrong!");
+        }
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
